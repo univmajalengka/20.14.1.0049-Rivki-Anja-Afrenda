33b4800cde62bc82ada73ce843a95530f2d640bd
Rivki Anja Afrenda <73579987+RIvkianjaafrenda@users.noreply.github.com>
2021-04-01 19:22:31 +0700

    Tugas Praktikum Alpro21

--- Tugas 1/Tugas1.1.Biodata_Singkat.pas
+++ Tugas 1/Tugas1.1.Biodata_Singkat.pas
@@ -13,7 +13,7 @@ begin
  
   nama   := '1. Nama   : Rivki Anja Afrenda';
   npm    := '2. NPM    : 20.14.1.0049';
-  Alamat := '3. Alamat : Desa Jatiserang blok.desa rt04 rw02 kec.panyingkiran kab.majalengka';
+  alamat := '3. Alamat : Desa Jatiserang blok.desa rt04 rw02 kec.panyingkiran kab.majalengka';
   ttl    := '4. TTL    : Majalengka, 24 juli 2001';
   nohp   := '5. No. HP : 08814517418';
   motto  := '6. Motto  : kebahagiaan itu terletak pada ketaatan kita kepala allah';
